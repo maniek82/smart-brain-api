7bf646e439ad4ab53fab791267b8d3a573e115f9
maniek82 <mariusz.tripler@gmail.com>
2018-09-14 22:38:21 +0100

    debugging

--- controllers/image.js
+++ controllers/image.js
@@ -6,16 +6,13 @@ const app = new Clarifai.App({
    });
    
 const handleApiCall = (req,res)=> {
-    app.models
-    .predict( Clarifai.FACE_DETECT_MODEL, req.body.input)
-    .then(data=> {
+    app.models.predict( Clarifai.FACE_DETECT_MODEL, req.body.input).then(data=> {
         res.json(data)
-    })
-    .catch(err=> res.status(400).json("unable to work with API"))
+    }).catch(err=> res.status(400).json("unable to work with API"))
 }
   
 
-const imageHandler = (req,res)=> {
+const imageHandler = (req, res, db)=> {
     const {id} = req.body;
     
     db('users')
@@ -24,8 +21,7 @@ const imageHandler = (req,res)=> {
         .returning('entries')
         .then(entries => {
             res.json(entries[0]);
-        })
-        .catch(err=> {
+        }).catch(err=> {
             res.status(400).json("unable to get entries");
         })
 }
--- server.js
+++ server.js
@@ -31,18 +31,18 @@ app.get('/',(req,res)=> {
     res.send("App is working");
 })
 
-app.post('/signin',(req,res)=>{signin.handleSignin(req,res,db,bcrypt)});
+app.post('/signin',(req,res) => {signin.handleSignin(req, res, db, bcrypt)});
 
-app.post('/register',(req,res)=>{register.handleRegister(req, res,db,bcrypt)})
+app.post('/register',(req,res) => {register.handleRegister(req, res, db, bcrypt)})
 
-app.get('/profile/:id',(req,res)=>{profile.handleProfile(req,res,db)})
-
-
+app.get('/profile/:id',(req,res) => {profile.handleProfile(req, res, db)})
 
 
-app.put('/image',(req,res)=>{image.imageHandler(req,res,db)})
 
-app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res)})
+
+app.put('/image',(req,res) => {image.imageHandler(req, res, db)})
+
+app.post('/imageurl',(req,res)=>{image.handleApiCall(req, res)})
 
 
 
